e6fe37f93d006b7319754f8185f11c752f1c7ce7
Arbaz Nagaria <arbaznagaria@gmail.com>
2022-01-03 23:24:07 +0530

    Added Menu and Search Icon Non Clickable

--- lib/main.dart
+++ lib/main.dart
@@ -5,23 +5,57 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MaterialApp(
+    title: 'My App',
+    home: SafeArea(child: MyScaffold()),
+  ));
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({required this.title, Key? key}) : super(key: key);
+  final Widget title;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My App',
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color(0xFF345DEE),
-          title: Text('PWM'),
-        ),
-        backgroundColor: Color(0xFF969755),
-        body: Center(
-          child: Text('Hey Mama!'),
-        ),
+    return Container(
+      height: 56.0,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.red),
+      child: Row(
+        children: [
+          const IconButton(
+            onPressed: null,
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation Menu',
+          ),
+          Expanded(child: title),
+          const IconButton(
+            onPressed: null,
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MyScaffold extends StatelessWidget {
+  const MyScaffold({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Column(
+        children: [
+          MyApp(
+            title: Text('Example Title',
+                style: Theme.of(context).primaryTextTheme.headline6),
+          ),
+          const Expanded(
+              child: Center(
+            child: Text('Hello World!'),
+          ))
+        ],
       ),
     );
   }
